72df580ef538a41c1bbe3c9f4d3c4f6b36a37f3f
Juliana Almeida <jualmeida2013@gmail.com>
2020-07-20 21:02:19 -0300

    commit

--- VR/features/page/busca_cep.rb
+++ VR/features/page/busca_cep.rb
@@ -3,28 +3,22 @@ class BuscarCep
     require 'httparty'
 
 
-
     def getBuscar(cep)  
   
         uri_base = "https://viacep.com.br/ws/<cep>/json" 
         uri_base["<cep>"] = cep
         $response = HTTParty.get(uri_base)
         
-        
     end
-
 
     def exibir()
 
         if $response.body.include? "\"erro\": true"    
             puts "CEP Inválido"
-  
         else
             puts "IBGE: #{$response["ibge"]}"
         end
  
     end
-
-
 
 end
